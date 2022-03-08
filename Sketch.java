735de3e96191f491a28310660756d9a47f23a31c
Joanne Kwong <99990333+joannekwong@users.noreply.github.com>
2022-03-08 15:09:11 +0000

    5.3 Processing Task 2 - Using Variables

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(200, 250);
   }
 
   /** 
@@ -16,20 +16,53 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(50, 129, 168);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  //foreground settings
+    stroke(0,0,0);
+    strokeWeight(1);
+    fill(50, 168, 74);
+    rect((float) (width/-20), (float) (height/1.06), (float) (width/0.9), (height));
+
+  //draw the head
+    fill(247, 231, 87);
+    ellipse(100, 100, 150, 150);
+    
+  //white
+    fill(255);
+    
+  //draw the eyes
+    ellipse(75, 85, 30, 20);
+    ellipse(125, 85, 30, 20);
+    
+  //black
+    fill(8, 10, 10);
+    
+  //draw the pupils
+    ellipse(75, 85, 10, 10);
+    ellipse(125, 85, 10, 10);
+    
+  //red
+    fill(255, 0, 0);
+    
+  //draw the mouth
+    arc(100, 125, 80, 50, 0, 311/99);
+    line(60, 125, 140, 125);
+
+  //lines for body
+    stroke(0, 0, 0);
+    strokeWeight(2);
+    line(100, 210, 100, 175);
+    line(130, 190, 70, 190);
+    line(120, 230, 100, 210);
+    line(100, 210, 80, 230);
+    
   }
   
   // define other methods down here.
